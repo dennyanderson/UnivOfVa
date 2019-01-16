0933a2e0bb76bcd29cd315e5774d8bde014a2b93
Denny Anderson <dra2zp@virginia.edu>
2019-01-15 19:17:01 -0500

    2019-01-15

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // DRA
 $(function() {
     var count = 0;
-    setInterval(function() {
+    var update = setInterval(function() {
 	var updateDiscussion = $.ajax({
 	    type: "POST",
 	    url: "controller.php?request=fetchDiscussion",
@@ -19,6 +19,59 @@ $(function() {
 		count = 0;
 		for (var i = 0; i < data.length; i++) {
 		    count++;
+		    if (data[i]["at"] != "" && data[i]["hashtag"] != "") {
+			$("#discussionContainer").append(
+			    '<p class="timestamp">' + data[i]["timestamp"] + '</p><p><strong>' + data[i]["first_name"] + ' (<a class="searchInPost" id="!' + data[i]["computing_id"] + '">' + data[i]["computing_id"] + '</a>) ' + data[i]["course"] + ' ' + data[i]["number"] + ' <a class="searchInPost" id="@' + data[i]["at"] + '">@' + data[i]["at"] + '</a> <a class="searchInPost" id="#' + data[i]["hashtag"] + '">#' + data[i]["hashtag"] + '</a></strong> ' + data[i]["post"] + '</p><hr>'
+			);
+		    }
+		    else if (data[i]["at"] != "" && data[i]["hashtag"] == "") {
+			$("#discussionContainer").append(
+			    '<p class="timestamp">' + data[i]["timestamp"] + '</p><p><strong>' + data[i]["first_name"] + ' (<a class="searchInPost" id="!' + data[i]["computing_id"] + '">' + data[i]["computing_id"] + '</a>) ' + data[i]["course"] + ' ' + data[i]["number"] + ' <a class="searchInPost" id="@' + data[i]["at"] + '">@' + data[i]["at"] + '</a></strong> ' + data[i]["post"] + '</p><hr>'
+			);
+		    }
+		    else if (data[i]["at"] == "" && data[i]["hashtag"] != "") {
+			$("#discussionContainer").append(
+			    '<p class="timestamp">' + data[i]["timestamp"] + '</p><p><strong>' + data[i]["first_name"] + ' (<a class="searchInPost" id="!' + data[i]["computing_id"] + '">' + data[i]["computing_id"] + '</a>) ' + data[i]["course"] + ' ' + data[i]["number"] + ' <a class="searchInPost" id="#' + data[i]["hashtag"] + '">#' + data[i]["hashtag"] + '</a></strong> ' + data[i]["post"] + '</p><hr>'
+			);
+		    }
+		    else {
+			$("#discussionContainer").append(
+			    '<p class="timestamp">' + data[i]["timestamp"] + '</p><p><strong>' + data[i]["first_name"] + ' (<a class="searchInPost" id="!' + data[i]["computing_id"] + '">' + data[i]["computing_id"] + '</a>) ' + data[i]["course"] + ' ' + data[i]["number"] + '</strong> ' + data[i]["post"] + '</p><hr>'
+			);
+		    }
+		}
+	    }
+	});
+	$(".searchInPost").click(function(evt) {
+	    clearInterval(update);
+	    var id = evt.target.id;
+	    var parent_node = document.getElementById("discussionContainer");
+	    if (parent_node) {
+		while (parent_node.firstChild) {
+		    parent_node.removeChild(parent_node.firstChild);
+		}
+	    }
+	    var at = "";
+	    var hashtag = "";
+	    if (id.substring(0, 1) == '!' || id.substring(0, 1) == '@') {
+		at = id.substring(1);
+	    }
+	    if (id.substring(0, 1) == "#") {
+		hashtag = id.substring(1);
+	    }
+	    var searchDiscussionFromHyperlink = $.ajax({
+		type: "POST",
+		url: "controller.php?request=fetchDiscussion",
+		data: {course: "", number: "", at: at, hashtag: hashtag, post: "%%"},
+		dataType: "json"
+	    });
+	    searchDiscussionFromHyperlink.done(function(data) {
+		if (parent_node) {
+		    while (parent_node.firstChild) {
+			parent_node.removeChild(parent_node.firstChild);
+		    }
+		}
+		for (var i = 0; i < data.length; i++) {
 		    if (data[i]["at"] != "" && data[i]["hashtag"] != "") {
 			$("#discussionContainer").append(
 			    '<p class="timestamp">' + data[i]["timestamp"] + '</p><p><strong>' + data[i]["first_name"] + ' (' + data[i]["computing_id"] + ') ' + data[i]["course"] + ' ' + data[i]["number"] + ' @' + data[i]["at"] + ' #' + data[i]["hashtag"] + '</strong> ' + data[i]["post"] + '</p><hr>'
@@ -40,10 +93,11 @@ $(function() {
 			);
 		    }
 		}
-	    }
+	    });
 	});
     }, 1000);
     $("#searchPosts").click(function(evt) {
+	clearInterval(update);
 	var course = $("#course").val();
 	var number = $("#number").val();
 	var at = $("#at").val();
@@ -141,7 +195,13 @@ $(function() {
 	    });
 	    postComment.done(function(data) {
 		if (data != "error") {
-		    window.location = 'index.php';
+		    $("#course").val("");
+		    $("#number").val("");
+		    $("#at").val("");
+		    $("#hashtag").val("");
+		    $("#post").val("");
+		    var count = 0;
+		    update;
 		}
 		else {
 		    alert("We have detected that you are attempting to perform a JavaScript or SQL injection, which is against the site's terms of use policy. We have temporarily disabled your account while it is being reviewed.");
@@ -149,5 +209,5 @@ $(function() {
 		}
 	    });
 	}
-    });	
+    });
 });
